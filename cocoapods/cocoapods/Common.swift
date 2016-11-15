9cf7945356bb9fa4cc000d042201f7f250ccfb52
chenshunyi <chenshunyi@vip.house365.com>
2016-11-15 15:29:22 +0800

    添加common类

---
+++ cocoapods/cocoapods/Common.swift
@@ -0,0 +1,13 @@
+//
+//  Common.swift
+//  cocoapods
+//
+//  Created by chenshunyi on 16/11/15.
+//  Copyright © 2016年 house365. All rights reserved.
+//
+
+import UIKit
+
+class Common: NSObject {
+
+}
